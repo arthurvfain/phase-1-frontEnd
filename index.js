51a48a7d4b77d25be166f25ba1f4dd14ed8eac5a
Arthur V Fain <arthurvfain@gmail.com>
2021-06-01 16:30:24 -0400

    starting frontend work

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 document.addEventListener('DOMContentLoaded',getStuff)
-let API_KEY = 'rYwtc500ImXhDQftDD2SbP96hTQsrzGQYYqs6WJZ'
-let parkCode = 'acad'
+// let API_KEY = 'rYwtc500ImXhDQftDD2SbP96hTQsrzGQYYqs6WJZ'
+// let parkCode = 'acad'
+// function getStuff()
+// {
+//     fetch(`https://developer.nps.gov/api/v1/parks?parkCode=${parkCode}&api_key=${API_KEY}`).then(resp=>resp.json()).then(console.log)
+// }
+
 function getStuff()
 {
-    fetch(`https://developer.nps.gov/api/v1/parks?parkCode=${parkCode}&api_key=${API_KEY}`).then(resp=>resp.json()).then(console.log)
+    fetch(`http://localhost:3000/data`).then(resp=>resp.json()).then(populateMenu)
 }
+
+function populateMenu(parkArray)
+{
+    parkArray.forEach(x=>console.log(x))
+}
+
+//likes to make top parks
